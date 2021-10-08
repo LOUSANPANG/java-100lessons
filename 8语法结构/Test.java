98ebc2fcfa6122733d90c5eed2dd89c034dae6ec
LOUSANPANG <1271255653@qq.com>
2021-10-08 08:50:45 +0800

    switch

--- 8语法结构/Test.java
+++ 8语法结构/Test.java
@@ -3,16 +3,40 @@
 
     分支结构
         if分支
-        switch分支
+        switch case
+            byte short int char enum String
     
     循环结构
         for
         while
         do while
+    
+    == 与 equals  xx.equals()
  */
+
+ import java.util.Scanner;
 
 public class Test {
     public static void main (String[] args) {
-        System.out.println();
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("请输入：");
+        // int month = input.nextInt();
+        // if (month == 1) {
+        //     System.out.println("已输入一");
+        // } else {
+        //     System.out.println("输入其他");
+        // }
+
+        int a = 1;
+        switch(a) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                system.out.println("3");
+        }
     }
 }
